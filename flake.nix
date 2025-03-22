178903c7d711e6a37fc235b17d6a165116f187d4
Manfred Endres <manfred.endres@tslarusso.de>
2025-03-22 23:32:55 +0100

    Hardcode VM disk

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "NixOS flake";
   
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     disko = {
@@ -22,7 +22,7 @@
         {
           disko.devices.disk.main = {
             # When using disko-install, we will overwrite this value from the commandline
-            device = "/dev/disk/by-id/some-disk-id";
+            device = "/dev/vda";
             type = "disk";
             content = {
               type = "gpt";
